f7a62d5297de62d7cac2898c67e3275d1e13c189
Chlamydomonos <2457242458@qq.com>
2026-02-20 15:45:37 +0800

    再次修改create-post

--- packages/cli/create-post.mjs
+++ packages/cli/create-post.mjs
@@ -3,16 +3,25 @@ import { program } from 'commander';
 import path from 'path';
 import fs from 'fs';
 
-const template = `---\n${stringify({ createdAt: new Date().getTime() })}\n---`;
 const basePath =
     process.env.SOURCE_WORK_DIR || path.resolve(import.meta.dirname, '..', 'frontend', 'public', 'sites', 'blog');
 
-program.option('-f, --file <fileName>', '要创建的文件名').parse(process.argv);
-const relativePath = program.file;
+program
+    .option('-f, --file <fileName>', '要创建的文件名')
+    .option('-t, --title [title]', '文章标题', '未命名')
+    .parse(process.argv);
+const opts = program.opts();
+const relativePath = opts.file;
 const absolutePath = path.resolve(basePath, relativePath);
 const dirName = path.dirname(absolutePath);
 if (!fs.existsSync(dirName)) {
     fs.mkdirSync(dirName, { recursive: true });
 }
 
+const obj = {
+    createdAt: new Date().getTime(),
+    title: opts.title,
+};
+
+const template = `---\n${stringify(obj)}---`;
 fs.writeFileSync(absolutePath, template, { encoding: 'utf-8' });
